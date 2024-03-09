cbb3ec9f75537747255b9d14557a9c9dd10e5f5a
Ondra T <ot199509@icloud.com>
2024-03-09 13:34:25 +0100

    Authenticator commit

--- App.js
+++ App.js
@@ -4,26 +4,28 @@ import PlanMenu from "./PlanMenu";
 import Recents from "./Recents";
 import Search from "./Search";
 import Group from "./Group";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import SettingsPage from "./SettingsPage";
 
-import { Amplify } from "aws-amplify";
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+import {
+  Authenticator,
+  useAuthenticato,
+  withAuthenticator,
+} from "@aws-amplify/ui-react-native";
 
 function SignOutButton() {
   const { signOut } = useAuthenticator();
   return <Button title="Sign Out" onPress={signOut} />;
 }
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-function App() {
-  return (
-    /*     <NavigationContainer>
+const App = () => {
+  /*     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{
@@ -36,12 +38,13 @@ function App() {
         <Stack.Screen name="Settings" component={SettingsPage} />
       </Stack.Navigator>
     </NavigationContainer> */
+  return (
     <Authenticator.Provider>
       <Authenticator>
         <SignOutButton />
       </Authenticator>
     </Authenticator.Provider>
   );
-}
+};
 
-export default App;
+export default withAuthenticator(App);
