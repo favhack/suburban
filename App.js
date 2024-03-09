8c95185a417acb59cf7d6887c8038258248ac8c1
Milan Horínek <horinek@milanfon.cz>
2024-03-09 18:17:47 +0100

    Vykopnut petrovo kód

--- App.js
+++ App.js
@@ -4,31 +4,43 @@ import PlanMenu from "./PlanMenu";
 import Recents from "./Recents";
 import Search from "./Search";
 import Group from "./Group";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import SettingsPage from "./SettingsPage";
 
-const Stack = createNativeStackNavigator();
+import {
+  Authenticator,
+  useAuthenticator,
+  withAuthenticator,
+} from "@aws-amplify/ui-react-native";
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="SignUp" component={SignUpPage} />
-        <Stack.Screen name="Settings" component={SettingsPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
+
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./src/amplifyconfiguration.json";
+
+Amplify.configure(amplifyconfig);
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button title="Sign Out" onPress={signOut} />;
 }
 
-export default App;
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SignOutButton />
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+};
+
+export default withAuthenticator(App);
