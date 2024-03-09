e3d8b6f8fc244a0e4f94162be3748d369a13e965
Ondra T <ot199509@icloud.com>
2024-03-09 21:53:42 +0100

    After some HomePage fixes commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-
-import LoginPage from "./LoginPage.js"
-import SignUpPage from "./SignUpPage.js"
-import HomePage from "./HomePage.js"
-import SettingsPage from "./SettingsPage.js"
+import LoginPage from "./LoginPage.js";
+import SignUpPage from "./SignUpPage.js";
+import HomePage from "./HomePage.js";
+import SettingsPage from "./SettingsPage.js";
+import Tags from "./Tags.js";
+import PlanContainer from "./PlanContainer.js";
+import Struktura from "./Struktura.js";
 
 import { Amplify } from "aws-amplify";
 import amplifyconfig from "./src/amplifyconfiguration.json";
@@ -26,32 +28,34 @@ import { signOut } from "aws-amplify/auth";
 //
 
 async function trySignOut() {
-	console.log("Signing out the current user");
-	try {
-		await signOut();
-		console.log("Signed out.");
-	} catch (error) {
-		console.log("Error signing out:", error);
-		//TODO: Toast for errors
-	}
+  console.log("Signing out the current user");
+  try {
+    await signOut();
+    console.log("Signed out.");
+  } catch (error) {
+    console.log("Error signing out:", error);
+    //TODO: Toast for errors
+  }
 }
-
 
 const Stack = createStackNavigator();
 
 const App = () => {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="Login" component={LoginPage} />
-				<Stack.Screen name="SignUp" component={SignUpPage} />
-				<Stack.Screen name="Home" component={HomePage} />
-				<Stack.Screen name="Settings" component={SettingsPage} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="SignUp" component={SignUpPage} />
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Settings" component={SettingsPage} />
+        <Stack.Screen name="Tags" component={Tags} />
+        <Stack.Screen name="PlanContainer" component={PlanContainer} />
+        <Stack.Screen name="Struktura" component={Struktura} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 trySignOut();
 
-export default App
+export default App;
